e5b5f64061df1cb02403740e886e23846f74f480
dfg129 <dfg129@gmail.com>
2022-01-25 11:12:10 -0500

    update repo name

--- bin/pipeline-hobbes.ts
+++ bin/pipeline-hobbes.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { PipelineHobbes } from '../lib/pipeline-hobbes';
 
 const app = new cdk.App();
-new PipelineHobbesStack(app, 'PipelineHobbesStack', {
+new PipelineHobbes(app, 'PipelineHobbes', {
     env: {
         account: '707338571369',
         region: 'us-east-1',
