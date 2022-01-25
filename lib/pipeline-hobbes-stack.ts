7b1433ad9dc22a57475efe8bb963fdd893f5ba09
dfg129 <dfg129@gmail.com>
2022-01-25 11:55:57 -0500

    update repo name

--- lib/pipeline-hobbes-stack.ts
+++ lib/pipeline-hobbes-stack.ts
@@ -9,7 +9,9 @@ export class PipelineHobbesStack extends cdk.Stack {
         const pipeline = new CodePipeline(this, 'PipelineHobbes', {
             pipelineName: 'S3Pipeline',
             synth: new ShellStep('Synth', {
-                input: CodePipelineSource.gitHub('dfg129/pipeline-hobbes', 'main'),
+                input: CodePipelineSource.gitHub('dfg129/pipeline-hobbes', 'main', {
+                  connectionArn: 'arn:aws:codestar-connections:us-east-1:707338571369:connection/dc99f243-92b7-4802-be99-45049d05c420',
+                }),
                 commands: ['npm ci', 'npm run build', 'npx cdk synth']
             })
         });
