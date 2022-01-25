a5a80ad241d4811ea17716934473131db2fec4f9
dfg129 <dfg129@gmail.com>
2022-01-25 11:14:55 -0500

    update repo name

--- lib/pipeline-hobbes-stack.ts
+++ lib/pipeline-hobbes-stack.ts
@@ -3,10 +3,10 @@ import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class PipelineHobbes extends cdk.Stack {
-    constructor(scope: Construct, id: String, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const pipeline = new CodePipeline(this, 'PipelineHobbes' {
+        const pipeline = new CodePipeline(this, 'PipelineHobbes', {
             pipelineName: 'S3Pipeline',
             synth: new ShellStep('Synth', {
                 input: CodePipelineSource.gitHub('dfg129/pipeline-hobbes', 'main'),
