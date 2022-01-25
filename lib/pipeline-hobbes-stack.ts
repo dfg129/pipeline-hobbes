d4305910ac9176ca756c62aefd52c751f0d461c7
dfg129 <dfg129@gmail.com>
2022-01-25 12:49:01 -0500

    add s3 test stage

--- lib/pipeline-hobbes-stack.ts
+++ lib/pipeline-hobbes-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { S3HobbesStage  } from './pipeline-s3-stage';
 
 export class PipelineHobbesStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
