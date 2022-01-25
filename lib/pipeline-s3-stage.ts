5495d285cf3c1140516c1660f3182937c64b4dcf
dfg129 <dfg129@gmail.com>
2022-01-25 12:52:00 -0500

    add s3 test stage

--- lib/pipeline-s3-stage.ts
+++ lib/pipeline-s3-stage.ts
@@ -6,7 +6,7 @@ export class S3HobbesStage extends cdk.Stage {
     constructor(scope: Construct, id: string, props?: cdk.StageProps) {
         super(scope, id, props);
 
-        const s3Stack == new S3HobbesStack(this, 'S3Stack');
+        const s3Stack = new S3HobbesStack(this, 'S3Stack');
     }
 
 }
